bdb3e80a2424c89ebf3b392af0035cdb2f77a8c2
Shamonti <67649277+Shamonti@users.noreply.github.com>
2024-02-17 03:03:31 +0600

    Added layouts and action

---
+++ src/actions/logout.js
@@ -0,0 +1,8 @@
+import { redirect } from 'react-router-dom';
+import { deleteItem } from '../helpers';
+
+export async function logoutAction() {
+  deleteItem({ key: 'userName' });
+
+  return redirect('/');
+}
